77102e2bc3aec2c106a0e137bf5dad9f3c4b56de
андреев вячеслав <stragas@mail.ru>
2022-12-25 23:07:56 +0700

    Третья ДЗ- 3 задания вместе.потерял логику выполнения

---
+++ src/OOPhw3/transport/Bus.java
@@ -0,0 +1,10 @@
+package OOPhw3.transport;
+
+public class Bus extends Transport{
+    public Bus(String brand, String model, int productionYear, String productionCountry,
+               String color, int maxSpeed) {
+        super(brand, model, productionYear, productionCountry, color, maxSpeed);
+
+
+    }
+}
